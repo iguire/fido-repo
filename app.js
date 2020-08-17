730361ec9462aac589e63a702bbcff213b5b58f8
toxtlej1 <Jesustoxtle@gmail.com>
2020-08-16 21:31:41 -0400

    New files

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieSession = require('cookie-session'),
+    cookieParser = require('cookie-parser'),
+    path = require('path'),
+    fs = require('fs'),
+    crypto = require('crypto'),
+    config = require('./backend/config.json'),
+    routes = require('./backend/index');
+
+let server;
+const app = express();
+
+if (config.https.enabled) {
+    var options = {
+        key: fs.readFileSync(config.https.keyFilePath),
+        cert: fs.readFileSync(config.https.certFilePath),
+        requestCert: false,
+        rejectUnauthorized: false
+    };
+    server = require('https').createServer(options, app);
+} else {
+    server = require('http').Server(app);
+}
+
+
+app.use(bodyParser.json());
+app.use(cookieSession({ name: 'session', keys: [crypto.randomBytes(32).toString('hex')], secure: false }))
+app.use(cookieParser())
+
+//Relying Party (RP) Web App
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+//Relying Party (RP) Server API
+app.use('/', routes)
+
+const port = config.port || 3001;
+server.listen(port);
+console.log(`Service listening on port ${port}`);
+
+module.exports = app;
